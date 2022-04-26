f015c481159d619e9e4d28f35b628dd4af9180a5
Nikita Lavrentev <nlavrentev93@gmail.com>
2022-04-26 16:46:33 +0400

    Lesson 2 added Todolist component separeted from app file

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,25 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+    const tasks1 = [
+        { id: 1, title: 'HTML&CSS', isDone: true },
+        { id: 2, title: 'JS', isDone: true },
+        { id: 3, title: 'React JS', isDone: true },
+
+    ];
+    const tasks2 = [
+        { id: 1, title: 'Hello world', isDone: true },
+        { id: 2, title: 'I am Happy', isDone: false },
+        { id: 3, title: 'Yo', isDone: true },
+
+    ];
     return (
         <div className="App">
-            <Todolist />
-            <Todolist />
-            <Todolist />
+            <Todolist title='What to learn' tasks={tasks1}/>
+            <Todolist title='Songs' tasks={tasks2}/>
+            <Todolist title='What to buy' tasks={tasks2}/>
         </div>
     );
-}
+};
 
 export default App;
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-export const Todolist = () => {
+type PropsType = {
+    title: string,
+    tasks: taskType[]
+}
+
+type taskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const Todolist = (props: PropsType) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
